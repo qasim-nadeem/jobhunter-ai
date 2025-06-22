129810cf9c28f8cf08837b463b84bdb281e9dbf1
Qasim Nadeem <qqnadeem@gmail.com>
2025-06-22 21:32:23 +0300

    Created bilter plat controllers

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
+use App\Http\Controllers\AIController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [AIController::class, 'CallAI']);
